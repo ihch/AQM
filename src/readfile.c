4a1d622dd0602bb6e35c8eef98cc0abb5ea8f7e5
nemusou <ryubi.102395@gmail.com>
2019-06-24 00:01:33 +0900

    feat: aqmファイル読み込み関数の実装

--- src/readfile.c
+++ src/readfile.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "aqm.h"
+#include "readfile.h"
 
 
 /***
@@ -16,9 +16,28 @@
  ***/
 int readfile(WaveData *waveData) {
   FILE *fp;
+  char readbuf[MAX_LINE_LENGTH] = {'\0'};
 
   if ((fp = fopen(INPUT_FILENAME, "r")) == NULL) {
-    // error処理
     return EXIT_FAILURE;
   }
+
+  int i = 0;
+  while (i < WAVEDATA_LENGTH && fgets(readbuf, MAX_LINE_LENGTH, fp) != NULL) {
+    waveData->data[i++] = strtol(readbuf, NULL, 0);
+  }
+
+  return EXIT_SUCCESS;
+}
+
+int test_read(void) {
+  WaveData waveData;
+  readfile(&waveData);
+
+  // for (int i = 0; i < WAVEDATA_LENGTH; i++) {
+  for (int i = 0; i < 100; i++) {
+    printf("%lf\n", waveData.data[i]);
+  }
+
+  return 0;
 }
---
+++ src/readfile.h
@@ -0,0 +1,9 @@
+#ifndef INCLUDED_READFILE
+#define INCLUDED_READFILE
+#include "aqm.h"
+
+#define MAX_LINE_LENGTH 256
+
+int readfile(WaveData *waveData);
+
+#endif // INCLUDED_READFILE
