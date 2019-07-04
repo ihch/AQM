f84a899d7966fcf5895219da5398f3bb93bf8a37
nemusou <ryubi.102395@gmail.com>
2019-07-04 10:15:15 +0900

    docs: acf_controlの使用例を書いた

--- src/acf.c
+++ src/acf.c
@@ -52,7 +52,35 @@ int example_acf(void) {
   }
 
   for (int i = 0; i < 10; i++) {
-    printf("%lf\n", acf(&echoData, ACF_INTERVAL * i, ACF_INTERVAL * i + ACF_RANGE, 0));
+  //  printf("%lf\n", acf(&echoData, ACF_INTERVAL * i, ACF_INTERVAL * i + ACF_RANGE, 0));
+  }
+
+  return 0;
+}
+
+int main(void) { 
+  EchoData echoData;
+  Acf nolag_acf[SEA_FLOOR_DEPTH / ACF_INTERVAL + 10];
+  Acf max_acf[SEA_FLOOR_DEPTH / ACF_INTERVAL + 10];
+
+  if (readfile(&echoData) == EXIT_FAILURE) {
+    return -1;
+  }
+  acf_control(&echoData, ACF_RANGE, ACF_INTERVAL, SEA_FLOOR_DEPTH, nolag_acf, max_acf);
+
+  for (int i = 0; i < SEA_FLOOR_DEPTH / ACF_INTERVAL - 1; i++) {
+    if (i != 0) printf(",");
+    printf("%lf", nolag_acf[i].acf_result);
+  }
+  printf("\n");
+  for (int i = 0; i < SEA_FLOOR_DEPTH / ACF_INTERVAL - 1; i++) {
+    if (i != 0) printf(",");
+    printf("%lf", max_acf[i].acf_result);
+  }
+  printf("\n");
+  for (int i = 0; i < SEA_FLOOR_DEPTH / ACF_INTERVAL - 1; i++) {
+    if (i != 0) printf(",");
+    printf("%d", max_acf[i].lag);
   }
 
   return 0;
